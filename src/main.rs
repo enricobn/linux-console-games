33c582701596f9607edd1b787bd3b694bde32cec
Enrico Benedetti <enrico_benedetti@yahoo.com>
2020-04-30 22:32:57 +0200

    Main menu

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@ extern crate serde_json;
 extern crate termion;
 
 use std::{io, thread};
-use std::io::{stdout, Write};
+use std::io::{stdin, stdout, Write};
 use std::io::Read;
 use std::time::Duration;
 
-use termion::async_stdin;
+use termion::{async_stdin, color};
+use termion::event::Key;
+use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
 use crate::persistence::HighScores;
@@ -21,6 +23,7 @@ mod grid;
 mod persistence;
 mod shape;
 mod tetris;
+mod tetrismain;
 
 // from https://stackoverflow.com/questions/55755552/what-is-the-rust-equivalent-to-a-try-catch-statement
 macro_rules! attempt { // `try` is a reserved keyword
@@ -38,8 +41,82 @@ macro_rules! attempt { // `try` is a reserved keyword
 fn main() {
     let mut stdout = stdout().into_raw_mode().unwrap();
 
+    write!(stdout,
+           "{}{}{}",
+           termion::clear::All,
+           termion::cursor::Goto(1, 1),
+           termion::cursor::Hide).unwrap();
+
+    let mut index: i8 = 0;
+
+    let menu = vec!("Tetris", "Snake", "Quit");
+
+    'outer: loop {
+        write!(stdout,
+               "{}{}{}Console games{}\r\n\n",
+               termion::clear::All,
+               termion::cursor::Goto(1, 1),
+               color::Bg(color::Red),
+               termion::style::Reset).unwrap();
+
+        for i in 0..menu.len() {
+            let menu_item = menu[i];
+
+            if i == menu.len() -1 {
+                write!(stdout, "\n\r");
+            }
+
+            if index == i as i8 {
+                write!(stdout, "{} ", color::Bg(color::Cyan)).unwrap();
+            } else {
+                write!(stdout, "{} ", termion::style::Reset).unwrap();
+            }
+            write!(stdout,
+                   "{}\r\n{}",
+                   menu_item,
+                   termion::style::Reset).unwrap();
+        };
+
+        stdout.flush().unwrap();
+
+        let stdin = stdin();
+
+        for c in stdin.keys() {
+            match c.unwrap() {
+                Key::Char('q') => {
+                    index = -1;
+                    break 'outer;
+                }
+                Key::Up => {
+                    index -= 1;
+                    if index < 0 {
+                        index = 0
+                    }
+                    break;
+                }
+                Key::Down => {
+                    index += 1;
+                    if index >= menu.len() as i8 {
+                        index = menu.len() as i8 - 1;
+                    }
+                    break;
+                }
+                Key::Char('\n') => break 'outer,
+                _ => break
+            };
+        }
+    }
+
+    if index < 0 {
+        write!(stdout,
+               "{}{}",
+               termion::clear::All,
+               termion::cursor::Show).unwrap();
+        return;
+    }
+
     attempt! {{
-        run(&mut stdout);
+        run(&mut stdout, index);
     } catch(e) {
         write!(stdout,
                "{}\n\r",
@@ -52,112 +129,13 @@ fn main() {
     }}
 }
 
-fn run<W: Write>(mut stdout: &mut W) -> io::Result<()> {
-    let mut scores = HighScores::read(".tetris")?;
-
-    write!(stdout,
-           "{}{}q to exit, left and right arrow to move{}down to rotate clockwise, up to rotate counterclockwise.\r\n{}",
-           termion::clear::All,
-           termion::cursor::Goto(1, 1),
-           termion::cursor::Goto(1, 2),
-           termion::cursor::Hide)?;
-
-    stdout.flush()?;
-
-    let mut tetris = Tetris::new(10, 20);
-
-    let mut stdin = async_stdin().bytes();
-
-    let mut score: u32 = 0;
-
-    print(&mut stdout, &mut tetris, score)?;
-
-    'outer: loop {
-        for _i in 0..40 {
-            let mut key_pressed = false;
-
-            let b = stdin.next();
-            if let Some(Ok(b'q')) = b {
-                break 'outer;
-            } else if let Some(Ok(b' ')) = b {
-                let (packed, new_tetris) = tetris.fall()?;
-                score += packed as u32 * 1000;
-                tetris = new_tetris;
-                key_pressed = true;
-            } else if let Some(Ok(27)) = b {
-                let b = stdin.next();
-                if let Some(Ok(91)) = b {
-                    let b = stdin.next();
-                    if let Some(Ok(68)) = b {
-                        tetris = tetris.left()?;
-                        key_pressed = true;
-                    } else if let Some(Ok(67)) = b {
-                        tetris = tetris.right()?;
-                        key_pressed = true;
-                    } else if let Some(Ok(65)) = b {
-                        tetris = tetris.rotate_left()?;
-                        key_pressed = true;
-                    } else if let Some(Ok(66)) = b {
-                        tetris = tetris.rotate_right()?;
-                        key_pressed = true;
-                    }
-                }
-            }
-
-            if key_pressed {
-                while stdin.next().is_some() {}
-
-                print(&mut stdout, &mut tetris, score)?;
-            }
-
-            thread::sleep(Duration::from_millis(10));
-        }
-
-        if let Ok(Some((packed, new_tetris))) = tetris.next() {
-            tetris = new_tetris;
-
-            score += packed as u32 * 1000;
-            print(&mut stdout, &mut tetris, score)?;
-        } else {
-            // game as ended
-            scores.add(score);
-            scores.save()?;
-
-            break 'outer;
-        }
+fn run<W: Write>(mut stdout: &mut W, index: i8) -> io::Result<()> {
+    if index == 0 {
+        tetrismain::run(&mut stdout)
+    } else if index == 1 {
+        println!("Snake");
+        Result::Ok(())
+    } else {
+        Result::Ok(())
     }
-    write!(stdout,
-           "{}{}Game over! Score: {}\n\r",
-           termion::clear::All,
-           termion::cursor::Show,
-           score)?;
-
-    Result::Ok(())
-}
-
-fn print<W: Write>(mut stdout: &mut W, tetris: &mut Tetris, score: u32) -> io::Result<()> {
-    write!(stdout,
-           "{}Score: {}",
-           termion::cursor::Goto(1, 3),
-           score)?;
-    clear_rec(stdout, 25, 5, 10, 5)?;
-    tetris.print_next_shape(stdout, 30, 5)?;
-    goto(&mut stdout, 1, 4)?;
-    tetris.print(&mut stdout)
-}
-
-fn goto<W: Write>(stdout: &mut W, x: u16, y: u16) -> io::Result<()> {
-    write!(stdout,
-           "{}",
-           termion::cursor::Goto(x, y))
-}
-
-pub fn clear_rec<W: Write>(stdout: &mut W, x: u8, y: u8, width: u8, height: u8) -> io::Result<()> {
-    let row = " ".repeat(width as usize);
-    write!(stdout, "{}", termion::style::Reset)?;
-    for iy in y..(y + height) {
-        goto(stdout, x as u16, iy as u16)?;
-        write!(stdout, "{}", row)?;
-    }
-    Result::Ok(())
 }
---
+++ src/tetrismain.rs
@@ -0,0 +1,119 @@
+use std::{io, thread};
+use std::io::{stdout, Write};
+use std::io::Read;
+use std::time::Duration;
+
+use termion::async_stdin;
+use termion::raw::IntoRawMode;
+
+use crate::persistence::HighScores;
+use crate::tetris::Tetris;
+
+pub fn run<W: Write>(mut stdout: &mut W) -> io::Result<()> {
+    let mut scores = HighScores::read(".tetris")?;
+
+    write!(stdout,
+           "{}{}q to exit, left and right arrow to move{}down to rotate clockwise, up to rotate counterclockwise.\r\n{}",
+           termion::clear::All,
+           termion::cursor::Goto(1, 1),
+           termion::cursor::Goto(1, 2),
+           termion::cursor::Hide)?;
+
+    stdout.flush()?;
+
+    let mut tetris = Tetris::new(10, 20);
+
+    let mut stdin = async_stdin().bytes();
+
+    let mut score: u32 = 0;
+
+    print(&mut stdout, &mut tetris, score)?;
+
+    'outer: loop {
+        for _i in 0..40 {
+            let mut key_pressed = false;
+
+            let b = stdin.next();
+            if let Some(Ok(b'q')) = b {
+                break 'outer;
+            } else if let Some(Ok(b' ')) = b {
+                let (packed, new_tetris) = tetris.fall()?;
+                score += packed as u32 * 1000;
+                tetris = new_tetris;
+                key_pressed = true;
+            } else if let Some(Ok(27)) = b {
+                let b = stdin.next();
+                if let Some(Ok(91)) = b {
+                    let b = stdin.next();
+                    if let Some(Ok(68)) = b {
+                        tetris = tetris.left()?;
+                        key_pressed = true;
+                    } else if let Some(Ok(67)) = b {
+                        tetris = tetris.right()?;
+                        key_pressed = true;
+                    } else if let Some(Ok(65)) = b {
+                        tetris = tetris.rotate_left()?;
+                        key_pressed = true;
+                    } else if let Some(Ok(66)) = b {
+                        tetris = tetris.rotate_right()?;
+                        key_pressed = true;
+                    }
+                }
+            }
+
+            if key_pressed {
+                while stdin.next().is_some() {}
+
+                print(&mut stdout, &mut tetris, score)?;
+            }
+
+            thread::sleep(Duration::from_millis(10));
+        }
+
+        if let Ok(Some((packed, new_tetris))) = tetris.next() {
+            tetris = new_tetris;
+
+            score += packed as u32 * 1000;
+            print(&mut stdout, &mut tetris, score)?;
+        } else {
+            // game as ended
+            scores.add(score);
+            scores.save()?;
+
+            break 'outer;
+        }
+    }
+    write!(stdout,
+           "{}Game over! Score: {}  \n\r",
+           termion::clear::All,
+           score)?;
+
+    Result::Ok(())
+}
+
+fn print<W: Write>(mut stdout: &mut W, tetris: &mut Tetris, score: u32) -> io::Result<()> {
+    write!(stdout,
+           "{}Score: {}",
+           termion::cursor::Goto(1, 3),
+           score)?;
+    clear_rec(stdout, 25, 5, 10, 5)?;
+    tetris.print_next_shape(stdout, 30, 5)?;
+    goto(&mut stdout, 1, 4)?;
+    tetris.print(&mut stdout)
+}
+
+fn goto<W: Write>(stdout: &mut W, x: u16, y: u16) -> io::Result<()> {
+    write!(stdout,
+           "{}",
+           termion::cursor::Goto(x, y))
+}
+
+pub fn clear_rec<W: Write>(stdout: &mut W, x: u8, y: u8, width: u8, height: u8) -> io::Result<()> {
+    let row = " ".repeat(width as usize);
+    write!(stdout, "{}", termion::style::Reset)?;
+    for iy in y..(y + height) {
+        goto(stdout, x as u16, iy as u16)?;
+        write!(stdout, "{}", row)?;
+    }
+    Result::Ok(())
+}
