a836906a14562767d1c122ca9671defa8cb3064b
Enrico Benedetti <enrico_benedetti@yahoo.com>
2020-05-17 13:52:49 +0200

    snake: move logic in Snake

--- src/snake/snake.rs
+++ src/snake/snake.rs
@@ -1,19 +1,31 @@
 use std::io;
 use std::io::Write;
 
-use crate::common::point::{Direction, Point};
+use rand::Rng;
+
 use crate::common::ioutils::print_border;
+use crate::common::point::{Direction, Point};
 
 pub struct Snake {
     width: u8,
     height: u8,
     direction: Direction,
     points: Vec<Point>,
+    food: Vec<Point>,
+    score: u32,
 }
 
 impl Snake {
-    pub fn new(width: u8, height: u8, direction: Direction) -> Snake {
-        Snake { width, height, points: vec!(Point::new(width as i8 / 2, height as i8 / 2)), direction }
+    pub fn new(width: u8, height: u8, direction: Direction, food_count: u8) -> Snake {
+        let mut food = Vec::new();
+        let mut rng = rand::thread_rng();
+
+        for _i in 0..food_count {
+            food.push(Point::new(rng.gen_range(0, width) as i8,
+                                 rng.gen_range(0, height) as i8));
+        }
+
+        Snake { width, height, points: vec!(Point::new(width as i8 / 2, height as i8 / 2)), direction, food, score: 0 }
     }
 
     pub fn last(&self) -> &Point {
@@ -21,10 +33,17 @@ impl Snake {
     }
 
     pub fn mv(&self, direction: Direction) -> Snake {
-        Snake { width: self.width, height: self.height, points: self.points.clone(), direction }
+        Snake {
+            width: self.width,
+            height: self.height,
+            points: self.points.clone(),
+            direction,
+            food: self.food.clone(),
+            score: self.score,
+        }
     }
 
-    pub fn next(&self, eat: bool) -> Option<Snake> {
+    pub fn next(&self) -> Option<Snake> {
         let last = self.points.last().unwrap();
 
         let point = last.mv(&self.direction);
@@ -35,23 +54,51 @@ impl Snake {
             return None;
         }
 
-        let mut points = self.points.clone();
+        let mut food: Vec<Point> = self.food.clone();
 
-        if !eat {
+        let food_found = self.food.iter().enumerate()
+            .find(|(_i, point)| point.x == self.last().x && point.y == self.last().y)
+            .map(|(i, _point)| i);
+
+        let mut rng = rand::thread_rng();
+
+        let mut points: Vec<Point> = self.points.clone();
+
+        let mut score = self.score;
+
+        if let Some(food_index) = food_found {
+            food.remove(food_index);
+            food.push(Point::new(rng.gen_range(0, self.width) as i8,
+                                 rng.gen_range(0, self.height) as i8));
+            score += 100;
+        } else {
             points.remove(0);
         }
 
         points.push(point);
 
-        Some(Snake { width: self.width, height: self.height, points, direction: self.direction.clone() })
+        Some(Snake {
+            width: self.width,
+            height: self.height,
+            points,
+            direction: self.direction.clone(),
+            food,
+            score,
+        })
     }
 
     pub fn print<W: Write>(&self, term: &mut W, x: u16, y: u16) -> io::Result<()> {
+        for point in self.food.iter() {
+            write!(term, "{}.", termion::cursor::Goto(point.x as u16 + 2, point.y as u16 + 3))?;
+        }
+
         for point in self.points.iter() {
             write!(term, "{}#", termion::cursor::Goto(point.x as u16 + x + 1, point.y as u16 + y + 1))?;
         }
         print_border(term, x, y, self.width as u16 + 2, self.height as u16 + 2)
     }
 
-
+    pub fn score(&self) -> u32 {
+        self.score
+    }
 }
--- src/snake/snakemain.rs
+++ src/snake/snakemain.rs
@@ -3,12 +3,11 @@ use std::io::{Error, Write, Read};
 use std::marker::PhantomData;
 use std::time::Duration;
 
-use rand::Rng;
 use termion::event::Key;
 use termion::input::TermRead;
 
 use crate::common::persistence::HighScores;
-use crate::common::point::{Direction, Point};
+use crate::common::point::Direction;
 use crate::Main;
 use crate::snake::snake::Snake;
 
@@ -32,15 +31,7 @@ impl<W: Write, R: Read> Main<W, R> for SnakeMain<W> {
     }
 
     fn run(&self, mut stdout: &mut W, stdin: &mut R) -> io::Result<Option<u32>> {
-        let mut snake = Snake::new(WIDTH, HEIGHT, Direction::East);
-        let mut score: u32 = 0;
-        let mut food: Vec<Point> = vec!();
-        let mut rng = rand::thread_rng();
-
-        for _i in 0..FOOD {
-            food.push(Point::new(rng.gen_range(0, WIDTH) as i8,
-                                 rng.gen_range(0, HEIGHT) as i8));
-        }
+        let mut snake = Snake::new(WIDTH, HEIGHT, Direction::East, FOOD);
 
         let mut result: io::Result<Option<u32>> = Result::Ok(None);
 
@@ -69,30 +60,17 @@ impl<W: Write, R: Read> Main<W, R> for SnakeMain<W> {
                 }
 
                 if key_pressed {
-                    print(&mut stdout, &snake, &food, score)?;
+                    print(&mut stdout, &snake)?;
                 }
 
                 thread::sleep(Duration::from_millis(10));
             }
 
-            if let Some(next_snake) = snake.next(false) {
-                let food_found = food.iter().enumerate()
-                    .find(|(_i, point)| point.x == next_snake.last().x && point.y == next_snake.last().y)
-                    .map(|(i, _point)| i);
-
-                if let Some(food_index) = food_found {
-                    food.remove(food_index);
-                    food.push(Point::new(rng.gen_range(0, WIDTH) as i8,
-                                         rng.gen_range(0, HEIGHT) as i8));
-                    snake = snake.next(true).unwrap();
-                    score += 10;
-                } else {
-                    snake = next_snake;
-                }
-
-                print(&mut stdout, &snake, &food, score)?;
+            if let Some(next_snake) = snake.next() {
+                snake = next_snake;
+                print(&mut stdout, &snake)?;
             } else {
-                result = Result::Ok(Some(score));
+                result = Result::Ok(Some(snake.score()));
                 break 'outer;
             }
         }
@@ -107,16 +85,12 @@ impl<W: Write, R: Read> Main<W, R> for SnakeMain<W> {
     }
 }
 
-fn print<W: Write>(mut stdout: &mut W, snake: &Snake, food: &Vec<Point>, score: u32) -> io::Result<()> {
+fn print<W: Write>(mut stdout: &mut W, snake: &Snake) -> io::Result<()> {
     write!(stdout,
            "{}{}Score: {}",
            termion::clear::All,
            termion::cursor::Goto(1, 1),
-           score)?;
-
-    for point in food.iter() {
-        write!(stdout, "{}.", termion::cursor::Goto(point.x as u16 + 2, point.y as u16 + 3))?;
-    }
+           snake.score())?;
 
     snake.print(&mut stdout, 1, 2)?;
 
